1ad9b871f3d4fd62b2f454e53fd3b4e2a037b9c1
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2023-12-05 15:49:18 -0500

    Test file

---
+++ src/test/java/dataTest/historicoCarteraSegMonto_ColocPorOF/HistoricoCarteraSegMonto_ColocPorOF.java
@@ -0,0 +1,285 @@
+package dataTest.historicoCarteraSegMonto_ColocPorOF;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.IOUtils;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
+
+import static dataTest.FunctionsApachePoi.*;
+import static dataTest.MethotsAzureMasterFiles.*;
+
+public class HistoricoCarteraSegMonto_ColocPorOF {
+    //110 hojas
+
+    private static String menu(List<String> opciones) {
+
+        JFrame frame = new JFrame("Menú de Opciones");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComboBox<String> comboBox = new JComboBox<>(opciones.toArray(new String[0]));
+        comboBox.setSelectedIndex(0);
+
+        JButton button = new JButton("Seleccionar");
+
+        ActionListener actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // Cerrar la ventana después de seleccionar una opción
+            }
+        };
+        button.addActionListener(actionListener);
+
+        JPanel panel = new JPanel();
+        panel.add(comboBox);
+        panel.add(button);
+
+        frame.add(panel);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+
+        while (frame.isVisible()) {
+            // Esperar hasta que la ventana se cierre
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return comboBox.getSelectedItem().toString();
+    }
+
+    public static void configuracion(String masterFile) {
+
+
+        JOptionPane.showMessageDialog(null, "Seleccione el archivo OkCartera");
+        String okCartera = getDocument();
+
+        JOptionPane.showMessageDialog(null, "A continuación se creará un archivo temporal " +
+                "\n Se recomienda seleccionar la carpeta \"Documentos\" para esta función...");
+        String tempFile = getDirectory() + "\\TemporalFile.xlsx";
+
+        try {
+            waitSeconds(10);
+            System.out.println("Espere el proceso de análisis va a comenzar...");
+            waitSeconds(5);
+
+            System.out.println("Espere un momento el análisis puede ser demorado...");
+            waitSeconds(5);
+
+
+
+            nuevosOficinas(okCartera, tempFile);
+
+            JOptionPane.showMessageDialog(null, "Archivos analizados correctamente...");
+            waitSeconds(10);
+
+            deleteTempFile(tempFile);
+        } catch (HeadlessException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Test
+    public static void test(){
+        JOptionPane.showMessageDialog(null, "Seleccione el archivo OkCartera");
+        String okCartera = getDocument();
+        IOUtils.setByteArrayMaxOverride(300000000);
+
+        List<String> sheetNames = obtenerNombresDeHojas(okCartera);
+        List<String> headers = new ArrayList<>();
+        List<Map<String, String>> datosFiltrados = new ArrayList<>();
+        List<String> camposDeseados = Arrays.asList("codigo_sucursal", "capital");
+
+        for (String sheetName :
+                sheetNames) {
+            System.out.println("SheetName: " + sheetName);
+            sheetName = "Hoja1";
+
+            List<String> encabezados = obtenerEncabezados(okCartera, sheetName);
+            for (String encabezado : encabezados) {
+                System.out.println("Header: " + encabezado);
+            }
+
+            String campoFiltrar = "tipo_cliente";
+            String valorInicio = "Nuevo"; // Reemplaza con el valor de inicio del rango
+            String valorFin = "Nuevo"; // Reemplaza con el valor de fin del rango
+
+            // Filtrar los datos por el campo y el rango especificados
+            datosFiltrados = obtenerValoresDeEncabezados(okCartera, sheetName, campoFiltrar, valorInicio, valorFin);
+            System.out.println("DATOSFILTRADOS: " + datosFiltrados.size() + " : " + datosFiltrados);
+
+            for (Map<String, String> rowData : datosFiltrados) {
+                System.out.println(rowData);
+                for (String campoDeseado : camposDeseados) {
+                    if (rowData.containsKey(campoDeseado)) {
+                        String valorCampo = rowData.get(campoDeseado);
+                        if (valorCampo != null) {
+                            System.out.println(campoDeseado + ": " + valorCampo);
+                        } else {
+                            System.out.println(campoDeseado + ": Valor nulo o campo no encontrado");
+                        }
+                    } else {
+                        System.out.println(campoDeseado + ": Campo no encontrado");
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static void nuevosOficinas(String okCarteraFile/*, String masterFile, String azureFile, String fechaCorte, String hoja*/, String tempFile) throws IOException {
+
+        IOUtils.setByteArrayMaxOverride(300000000);
+
+        //try {
+
+
+            List<String> sheetNames = obtenerNombresDeHojas(okCarteraFile);
+
+            List<String> headers = null;
+            List<Map<String, String>> datosFiltrados = null;
+            List<String> camposDeseados = Arrays.asList("codigo_sucursal", "capital");
+            for (String sheetName : sheetNames) {
+                System.out.println("Contenido de la hoja: " + sheetName);
+                headers = obtenerEncabezados(okCarteraFile, sheetName);
+
+                // Listar campos disponibles
+                System.out.println("Campos disponibles:");
+                for (String header : headers) {
+                    System.out.println(header);
+                }
+                // Especifica el campo en el que deseas aplicar el filtro
+                String campoFiltrar = "tipo_cliente";
+                String valorInicio = "Nuevo"; // Reemplaza con el valor de inicio del rango
+                String valorFin = "Nuevo"; // Reemplaza con el valor de fin del rango
+
+                // Filtrar los datos por el campo y el rango especificados
+                datosFiltrados = obtenerValoresDeEncabezados(okCarteraFile, sheetName, campoFiltrar, valorInicio, valorFin);
+
+
+                // Especifica los campos que deseas obtener
+
+
+                // Imprimir datos filtrados
+                System.out.println("DATOS FILTRADOS");
+                //System.out.println("Datos filtrados por " + campoFiltrar + " en el rango [" + valorInicio + ", " + valorFin + "]");
+                for (Map<String, String> rowData : datosFiltrados) {
+                    for (String campoDeseado : camposDeseados) {
+                        String valorCampo = rowData.get(campoDeseado);
+
+                        System.out.println(campoDeseado + ": " + valorCampo);
+                    }
+                    System.out.println();
+                }
+                runtime();
+                waitSeconds(2);
+
+
+            }
+            System.out.println("-----------CREACION TEMPORAL-----------");
+
+            // Crear una nueva hoja Excel con los datos filtrados
+            crearNuevaHojaExcel(tempFile, camposDeseados, datosFiltrados);
+
+            /*System.out.println("Analisis archivo temporal----------------------");
+
+            sheetNames = obtenerNombresDeHojas(tempFile);
+
+            for (String sheetName : sheetNames) {
+                System.out.println("Contenido de la hoja: " + sheetName);
+
+                headers = obtenerEncabezados(tempFile, sheetName);
+
+                System.out.println("Campos disponibles " + headers);
+
+                for (String header : headers) {
+                    System.out.println(header);
+                }
+
+
+                //List<String> camposDeseados = Arrays.asList("codigo_sucursal", "capital");
+                datosFiltrados = obtenerValoresDeEncabezados(tempFile, sheetName, camposDeseados);
+
+
+                System.out.println("VALORES DEL OK CARTERA");
+                for (Map<String, String> rowData : datosFiltrados) {
+                    for (String campoDeseado : camposDeseados) {
+                        String valorCampo = rowData.get(campoDeseado);
+                        System.out.println(campoDeseado + ": " + valorCampo);
+                    }
+                    System.out.println();
+                }
+                runtime();
+                waitSeconds(2);
+
+                Map<String, String> resultado = functions.calcularSumaPorValoresUnicos(tempFile, camposDeseados.get(0), camposDeseados.get(1));
+
+                List<Map<String, String>> datosMasterFile = obtenerValoresEncabezados2(azureFile, masterFile, hoja, fechaCorte)*//*getHeadersMFile(azureFile, masterFile, fechaCorte)*//*;
+
+
+                for (Map.Entry<String, String> entryOkCartera : resultado.entrySet()) {
+                    for (Map<String, String> datoMF : datosMasterFile) {
+                        for (Map.Entry<String, String> entry : datoMF.entrySet()) {
+                            *//*------------------------------------------------------------*//*
+                            if (entryOkCartera.getKey().contains(entry.getKey())) {
+
+                                System.out.println("CODIGO ENCONTRADO");
+
+
+                                if (!entryOkCartera.getValue().equals(entry.getValue())) {
+
+                                    System.out.println("LOS VALORES ENCONTRADOS SON DISTINTOS-> " + entryOkCartera.getValue() + ": " + entry.getValue() + " CON RESPECTO AL CODIGO: " + entry.getKey());
+                                } else {
+
+                                    System.out.println("LOS VALORES ENCONTRADOS SON IGUALES-> " + entryOkCartera.getValue() + ": " + entry.getValue() + " CON RESPECTO AL CODIGO: " + entry.getKey());
+
+                                }
+                            } else {
+                                System.err.println("Código no encontrado: " + entryOkCartera.getKey());
+                            }
+                            *//*-------------------------------------------------------------------*//*
+                        }
+                        waitSeconds(5);
+                        runtime();
+                    }
+
+                }
+                runtime();
+
+            }*/
+        /*} catch (IOException e) {
+            throw new RuntimeException("Error interno del proceso", e);
+        }*/
+    }
+
+
+}
